93351d74788cb39da684ef40e5b1c6d60edea47d
DevvMarko <devvmarko@gmail.com>
2025-09-02 00:07:25 +0200

    add try and new window

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,9 +1,13 @@
-import {app, BrowserWindow, ipcMain} from 'electron';
+import {app, BrowserWindow, Menu, Tray} from 'electron';
 import process from 'process';
 import path from 'path';
 
+
+// ==== variables ====
 let mainWindow;
 
+
+// ==== functions ====
 function createWindow() {
     mainWindow = new BrowserWindow({
         transparent: true,
@@ -13,20 +17,46 @@ function createWindow() {
             nodeIntegration: false,
             contextIsolation: true,
             preload: path.join(app.getAppPath(), 'src/electron/preload.js')
-        }
+        },
+        icon: path.join(app.getAppPath(), 'src/assets/app_logo.png')
     });
     
-    // mainWindow.setIgnoreMouseEvents(true);
     if(process.env.NODE_ENV === 'development') {
         mainWindow.loadURL('http://localhost:5173');
     }else{
         mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
     }
-    mainWindow.webContents.openDevTools();
 
+    // Make the window click-through and always on top
+    mainWindow.setIgnoreMouseEvents(true, { forward: true });
+    mainWindow.setAlwaysOnTop(true, 'screen-saver');
 }
-app.whenReady().then(createWindow)
 
-ipcMain.on("set-clickable", (event, isClickable) => {
-    mainWindow.setIgnoreMouseEvents(!isClickable, { forward: true });
-  });
+function optionsWindow() {
+    const optionsWin = new BrowserWindow({
+        width: 400,
+        height: 300,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            preload: path.join(app.getAppPath(), 'src/electron/preload.js')
+        },
+        icon: path.join(app.getAppPath(), 'src/assets/app_logo.png')
+    });
+    optionsWin.loadFile(path.join(app.getAppPath(), 'options.html'));
+}
+
+
+// ==== APP READY ====
+app.whenReady().then(() => {
+    createWindow();
+
+    const tray = new Tray(path.join(app.getAppPath(), 'src/assets/app_logo.png'));
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Options', click: () => optionsWindow() },
+        { label: 'Quit', role: 'quit' }
+    ]);
+    tray.setContextMenu(contextMenu);
+    tray.setToolTip('MiniMate');
+});
